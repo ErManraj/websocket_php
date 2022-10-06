cffd6f099b6c3ba892c0dfb31bc48a0a4b619388
ErManraj <manraj_singh@live.com>
2022-10-06 09:18:37 +0530

    codeing to create clean websocket in php js

---
+++ websockets.php
@@ -0,0 +1,21 @@
+<?php
+
+// $ composer require react/http react/socket # install example using Composer
+// $ php example.php # run example on command line, requires no additional web server
+
+require __DIR__ . '/vendor/autoload.php';
+
+
+
+
+$http = new React\Http\HttpServer(function (Psr\Http\Message\ServerRequestInterface $request) {
+	
+	 
+	$times = date('m/d/Y h:i:s a', time());
+    return React\Http\Message\Response::plaintext($times);
+});
+
+$socket = new React\Socket\SocketServer('127.0.0.1:8080');
+$http->listen($socket);
+
+echo "Server running at http://127.0.0.1:8080" . PHP_EOL;
